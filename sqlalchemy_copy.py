f13b5387f608c437a8ec68f427f0b8e5ede373d0
Remi Rampin <remi@rampin.org>
2022-11-02 09:49:54 -0400

    Retry transactions on SERIALIZATION_FAILURE

--- sqlalchemy_copy.py
+++ sqlalchemy_copy.py
@@ -1,8 +1,51 @@
 import argparse
 import itertools
+import random
 import sqlalchemy
+import sqlalchemy.exc
 from sqlalchemy.sql.functions import count
 import sys
+import time
+
+
+backoff_idx = -1
+backoff_amount = 0
+
+def exponential_backoff(idx, rand=random):
+    global backoff_idx
+    global backoff_amount
+    if idx != backoff_idx:
+        # Reset delay if we have made progress
+        backoff_amount = 0.1
+    else:
+        # Increase delay if we haven't
+        backoff_amount *= 2
+
+    # Random jitter
+    delay = rand.uniform(backoff_amount / 2, backoff_amount)
+
+    return delay
+
+
+error_handlers = []
+
+
+try:
+    import psycopg2
+except ImportError:
+    pass
+else:
+    import psycopg2.errorcodes
+
+    @error_handlers.append
+    def retry_on_serialization_failure(idx, err, attempt):
+        if (
+            attempt < 3
+            and isinstance(err, psycopg2.OperationalError)
+            and err.orig.pgcode == psycopg2.errorcodes.SERIALIZATION_FAILURE
+       ):
+            time.sleep(exponential_backoff(idx))
+            return True
 
 
 def main():
@@ -79,7 +122,24 @@ def main():
                 ]
                 if not batch:
                     break
-                tgt.execute(insert_stmt.values(batch))
+                attempt = 0
+                while True:
+                    try:
+                        with tgt.begin():
+                            tgt.execute(insert_stmt.values(batch))
+                    except sqlalchemy.exc.DatabaseError as e:
+                        handled = False
+                        for handler in error_handlers:
+                            if handler(idx, e, attempt):
+                                attempt += 1
+                                handled = True
+                                break
+                        if handled:
+                            continue
+                        else:
+                            raise
+                    else:
+                        break
                 idx += len(batch)
                 print('%d / %d' % (idx, total_rows))
 
